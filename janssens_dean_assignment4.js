648e1e8e94d7b29bf32e819b52dd44bbe07e93d6
Dean Janssens <djanssens@fullsail.edu>
2014-06-22 18:04:23 -0500

    added first function getNumber

--- janssens_dean_assignment4.js
+++ janssens_dean_assignment4.js
@@ -1,1 +1,21 @@
 //alert("JavaScript works!");
+var stringToNumber = "40";
+var number1 = 5.2356;
+var stringSeparate = "a,b,c";
+var url = "http://www.fullsail.edu";
+
+//Return String as a Number
+var getNumber = function (theNumber) {
+    var result = parseInt(theNumber);
+    return result;
+};
+
+
+
+
+var myResult = getNumber(stringToNumber);
+
+
+
+
+console.log(myResult);

840f8cc232a160e521d3ecd84dbd2106c5cd7c66
Dean Janssens <djanssens@fullsail.edu>
2014-06-23 17:57:34 -0500

    added getNextHighest function

--- janssens_dean_assignment4.js
+++ janssens_dean_assignment4.js
@@ -3,6 +3,7 @@ var stringToNumber = "40";
 var number1 = 5.2356;
 var stringSeparate = "a,b,c";
 var url = "https://www.fullsail.edu";
+var highestValue = [1, 4, 7, 9, 10, 14, 15];
 
 //Return String as a Number
 var getNumber = function (theNumber) {
@@ -23,24 +24,41 @@ var getDecimal = function (number1, decimalPlaces) {
     return decimalResult;
 };
 
+//Is the string a URL?
 var getUrl = function (myURL) {
-    var isURL;
+    var isaURL;
     if (myURL.substring(0, 7) === "http://" || myURL.substring(0, 8) === "https://") {
-        isURL = true;
+        isaURL = true;
     } else {
-        isURL = false;
+        isaURL = false;
     }
-    return isURL;
+    return isaURL;
 
 };
+
+//Find smallest value in an array that is greater than a given number
+var getNextHighest = function (arrayValue, number) {
+    arrayValue.sort();
+    for (var i = 0; i < arrayValue.length; i++) {
+        if (arrayValue[i] > number) {
+            return arrayValue[i];
+        }
+    }
+};
+
+
+
 
 var myResult = getNumber(stringToNumber);
 var myNewString = getNewString = getNewString(stringSeparate, ",", "/");
 var decimalPlaces = getDecimal(number1, 2);
 var isUrlValid = getUrl(url);
+var nextHighestNumber = getNextHighest(highestValue, 12);
+
 
 
 console.log(myResult);
 console.log(myNewString);
 console.log(decimalPlaces);
 console.log(isUrlValid);
+console.log(nextHighestNumber);
